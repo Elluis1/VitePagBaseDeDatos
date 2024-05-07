7192c0c26e791c3d4617687202729c0c66393d3d
luigi <luigimete54@gmail.com>
2024-05-07 09:56:22 -0300

    Intento filtro dias

--- src/pestañas/Agenda.jsx
+++ src/pestañas/Agenda.jsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
+import {listaCitas} from '../Archivos/Citas'
 import "../App";
 
 export function Agenda() {
   const [filtletra, setFiltletra] = useState("");
-  const [listaCitas, setCitas] = useState([]);
-  const [nombre, setNombre] = useState("");
-  const [time, setTime] = useState("");
-  const [day, setDay] = useState("");
-  const [consult, setConsult] = useState("");
-  const [id, setId] = useState(Date.now);
+  const [filtdia, setFiltdia] = useState([""])
+  // const [listaCitas, setCitas] = useState([]);
+  // const [nombre, setNombre] = useState("");
+  // const [time, setTime] = useState("");
+  // const [day, setDay] = useState("");
+  // const [consult, setConsult] = useState("");
+  // const [id, setId] = useState(Date.now);
 
-  const addCita = () => {
-    setId(id);
-    setNombre(nombre);
-    setTime(time);
-    setDay(day);
-    setConsult(consult);
-  };
+  // const addCita = () => {
+  //   setId(id);
+  //   setNombre(nombre);
+  //   setTime(time);
+  //   setDay(day);
+  //   setConsult(consult);
+  // };
 
-  setCitas(addCita());
+  // setCitas(addCita());
 
   // const nuevaCita = {
   //   id: 1,
@@ -28,54 +30,35 @@ export function Agenda() {
   //   consulta: consult,
   // }
 
-  const borrarCita = (id) => {
-    const citasFiltradas = listaCitas.filter((cita) => cita.id !== id);
-  };
+  // const borrarCita = (id) => {
+  //   const citasFiltradas = listaCitas.filter((cita) => cita.id !== id);
+  // };
 
   return (
     <section>
-      <h2>
-        Agregar Cita
-        <form>
-          <input
-            placeholder="Nombre del paciente"
-            onChange={(e) => setNombre(e.target.value)}
-          />
-          <input
-            placeholder="Hora"
-            onChange={(e) => setTime(e.target.value)}
-          />
-          <input
-            placeholder="Fecha"
-            onChange={(e) => setDay(e.target.value)}
-          />
-          <input
-            placeholder="Razon de consulta"
-            onChange={(e) => setConsult(e.target.value)}
-          />
-          <button type="submit" onClick={setCitas()}>Guardar</button>
-        </form>
-      </h2>
       <h1>
         Citas pendientes
       </h1>
+      <input type="checkbox" placeholder="Lunes" onClick={() => setFiltdia("Lunes") }/>
+      <input type="checkbox" placeholder="Martes" onClick={() => setFiltdia("Martes") }/>
+      <input type="checkbox" placeholder="Miercoles" onClick={() => setFiltdia("Miercoles") }/>
+      <input type="checkbox" placeholder="Jueves" onClick={() => setFiltdia("Jueves") }/>
+      <input type="checkbox" placeholder="Viernes" onClick={() => setFiltdia("Viernes") }/>
+      <input type="checkbox" placeholder="Sabado" onClick={() => setFiltdia("Sabado") }/>
       <input
         onChange={(e) => setFiltletra(e.target.value)}
         type="name"
         className="inputs"
         placeholder="Ingrese el nombre del paciente"
       />
-      {!(listaCitas.length < 1) ? <h1>No hay citas</h1> : <listaCitas.map />}
       <div className="citas">
         {listaCitas.map((elemento) =>
-          !(elemento.paciente === filtletra) ? (
+          !(elemento.dia === filtdia) ? (
             <h1 key={elemento.id} />
           ) : (
             <li key={elemento.id}>
-              {" "}
-              Nombre:{elemento.paciente} - Fecha: {elemento.fecha} - Hora:{" "}
-              {elemento.hora} - Razon de la consulta: {elemento.consulta}{" "}
-              <button className="error" onClick={borrarCita(elemento.id)}>
+              Nombre: {elemento.paciente} - Fecha: {elemento.fecha} - Dia: {elemento.dia} - Hora: {elemento.hora} - Razon de la consulta: {elemento.consulta}
+              <button className="error" >
                 Eliminar cita
               </button>
             </li>
@@ -85,3 +68,52 @@ export function Agenda() {
     </section>
   );
 }
+
+// return (
+//   <section>
+//     <h1>Citas pendientes</h1>
+//     <form onChange={(e) => setFiltletra(e.target.value)}>
+//       <input
+//         type="name"
+//         className="inputs"
+//         placeholder="Ingrese el nombre del paciente"
+//       />
+//       <div className="citas">
+//         {listaCitas.map((elemento) =>
+//           !(elemento.paciente === filtletra) ? (
+//             <h1 key={elemento.id}/>
+//           ) : (
+//             <li key={elemento.id}>
+//               {" "}
+//               Nombre:{elemento.paciente} - Fecha: {elemento.fecha} - Hora:{" "}
+//               {elemento.hora} - Razon de la consulta: {elemento.consulta}{" "}
+//             </li>
+//           )
+//         )}
+//       </div>
+//     </form>
+//   </section>
+// );
+
+{/*   <h2>
+        Agregar Cita
+        <form>
+          <input
+            placeholder="Nombre del paciente"
+            // onChange={(e) => setNombre(e.target.value)}
+          />
+          <input
+            placeholder="Hora"
+            // onChange={(e) => setTime(e.target.value)}
+          />
+          <input
+            placeholder="Fecha"
+            // onChange={(e) => setDay(e.target.value)}
+          />
+          <input
+            placeholder="Razon de consulta"
+            // onChange={(e) => setConsult(e.target.value)}
+          />
+          <button type="submit" >Guardar</button>
+        </form>
+      </h2> */}
